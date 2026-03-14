071f2675b6ee3373a462c013e3d57109613e8f56
Sneider_Lindarte <darwinlindarte072@gmail.com>
2026-03-14 09:12:52 -0500

    creacion de la primera pagina web

---
+++ src/pageweb/ejercicio.js
@@ -0,0 +1,34 @@
+console.log("hola")
+const getData = () => {
+    let cargador = document.getElementById("Cargando")
+    let info = document.getElementById("info")
+    cargador.classList.remove("ocultar")
+    info.classList.add("ocultar")
+   fetch("https://jsonplaceholder.typicode.com/users")
+       .then(res => res.json())
+       .then(data => {
+           let body= document.getElementById("body")
+           let a = ``;
+           data.forEach(element => {
+               a += ` 
+                    <tr>
+                        <td> ${element["id"]}</td> 
+                        <td> ${element["name"]}</td>
+                        <td> ${element["phone"]}</td>
+                        <td> ${element["username"]}</td>  
+                        <td> ${element["website"]}</td>
+                    </tr>
+               `
+           })
+           body.innerHTML = a
+           cargador.classList.add("ocultar")
+           info.classList.remove("ocultar")
+           console.log("hola")
+       })
+
+
+
+}
+document.getElementById("data")
+    .addEventListener("click", getData);
+
